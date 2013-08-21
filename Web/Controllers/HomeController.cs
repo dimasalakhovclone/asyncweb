17564cb18dcdb959a3ded3faa097fb9ba90f72b7
Dima Salakhov <to@dimasalakhov.com>
2013-08-21 23:48:26 +0600

    create empty mvc4 project

---
+++ Web/Controllers/HomeController.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Web.Mvc;
+
+namespace Web.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult GetSyncBlock()
+        {
+            Thread.Sleep(new Random().Next(1000, 10000));
+
+            return Json(new {date = DateTime.Now.ToString()}, JsonRequestBehavior.AllowGet);
+        }
+
+        public async Task<ActionResult> GetAsyncBlock()
+        {
+            await Task.Factory.StartNew(() => Thread.Sleep(new Random().Next(1000, 10000)));
+
+            return Json(new { date = DateTime.Now.ToString() }, JsonRequestBehavior.AllowGet);
+        }
+    }
+}   
---
+++ Web/Extensions/UrlExtensions.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq.Expressions;
+using System.Web.Mvc;
+using System.Web.Routing;
+
+namespace Web.Extensions
+{
+    public static class UrlExtensions
+    {
+        public static string Action<TController>(this UrlHelper helper, Expression<Action<TController>> action) where TController : Controller
+        {
+            var valuesFromExpression = Microsoft.Web.Mvc.Internal.ExpressionHelper.GetRouteValuesFromExpression<TController>(action);
+
+            var _controller = valuesFromExpression["Controller"] as string;
+            var _action = valuesFromExpression["Action"] as string;
+
+            return UrlHelper.GenerateUrl(null, _action, _controller, valuesFromExpression, helper.RouteCollection, helper.RequestContext, true);
+        }
+    }
+}
